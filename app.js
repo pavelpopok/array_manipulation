1bb469f821e8ed2528164243116cd11d0fe4e551
Paul Pok <pavel.popok@gmail.com>
2022-01-30 17:40:02 +0200

    create function that splits array

---
+++ app.js
@@ -0,0 +1,11 @@
+function chunkArray(ls, k, t) {
+    let outputArr = [];
+    for (let i = 0; i < ls.length; i += k) {
+       outputArr.push(ls.slice(i, k + i));
+    }
+    return  outputArr;
+   }
+
+
+
+
